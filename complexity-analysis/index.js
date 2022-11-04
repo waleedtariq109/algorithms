6a3f941f3e46f8fe441f47e92ec2cc3fac422ec3
Waleed Tariq <76990392+WaleedTariq109@users.noreply.github.com>
2022-11-04 05:15:57 +0500

    Create index.js

---
+++ complexity-analysis/index.js
@@ -0,0 +1,11 @@
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
+
+//* n = input_size => n = data
+
+// O(n) => O => Big O Notation
+
+// Complexity Values
